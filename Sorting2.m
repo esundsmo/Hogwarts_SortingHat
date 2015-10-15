6045e39b349ffb36733028cdd9b80342c0991a6b
jaycesheachow <jayce.chow@students.olin.edu>
2015-10-14 21:44:34 -0400

    Functions...
    We have functions to do everything (and a break statement after one
    iteration), but the functions aren’t working properly.

--- Sorting2.m
+++ Sorting2.m
@@ -1,4 +1,4 @@
-function sort()
+function Sorting2()
 % Use ctrl+enter to run
 % Generates a matrix of numbered students with their house aptitudes
 % House poistioning is officialy [G S R H]
@@ -36,13 +36,15 @@ end
 % Student_List = [transpose(N),S]
 Student_List = S;
 
-for m = 1:1:n
-    [smax(m),pos(m)] = max(Student_List(m,2:5));
-end
 
 [GR,SL,RA,HU,Student_List] = file(Student_List);
 
+% Initial Sort
 function [GR,SL,RA,HU,MAT] = file(MAT)
+    % Takes the maximun house value for each student
+    for m = 1:1:n
+        [smax(m),pos(m)] = max(MAT(m,2:5));
+    end
     for h = 1:n
         if pos(h) == 1
             GR(g_c,:) = MAT(h,:);
@@ -63,19 +65,6 @@ function [GR,SL,RA,HU,MAT] = file(MAT)
         end
     end
 end
-
-% House Matrices
-% GR
-% SL
-% RA
-% HU
-[rg,cg] = size(GR);
-[rs,cs] = size(SL);
-[rr,cr] = size(RA);
-[rh,ch] = size(HU);
-
-
-%% Now To Size Check...
 
 % Function for removing extras
 function rejects = remove(GR,SL,RA,HU)
@@ -114,8 +103,67 @@ function rejects = remove(GR,SL,RA,HU)
     end
 end
 
+% REcursive Sorting?
+function [GR,SL,RA,HU] = REsort(GR,SL,RA,HU,REmat)
+    [row,col] = size(REmat); % Gets size of reject matrix
+    for m = 1:1:row
+        [smax(m),pos(m)] = max(REmat(m,2:5));
+    end
+    % +1 because we don't want to overwrite the last value
+    [Gr,Gc] = size(GR); Gr = Gr+1;
+    [Sr,Sc] = size(SL); Sr = Sr+1;
+    [Rr,Rc] = size(RA); Rr = Rr+1;
+    [Hr,Hc] = size(HU); Hr = Hr+1;
+    for h = 1:length(pos)
+        if pos(h) == 1
+            GR(Gr,:) = REmat(h,:);
+%             REmat(h,6) = 'G';
+            Gr = Gr + 1;
+        elseif pos(h) == 2
+            SL(Sr,:) = REmat(h,:);
+            Sr = Sr + 1;
+%             REmat(h,6) = 'S';
+        elseif pos(h) == 3
+            RA(Rr,:) = REmat(h,:);
+%             REmat(h,6) = 'R';
+            Rr = Rr + 1;
+        elseif pos(h) == 4
+            HU(Hr,:) = REmat(h,:);
+%             REmat(h,6) = 'H';
+            Hr =  + 1;
+        end
+    end
+end
 
-% rejects
+
+% House Matrices
+% GR
+% SL
+% RA
+% HU
+
+
+
+%% Now To Size Check...
+x=6; % stupid variable to keep the while loop
+while x==6
+    [rG,cG] = size(GR);
+    [rS,cS] = size(SL);
+    [rR,cS] = size(RA);
+    [rH,cH] = size(HU);
+    if rG == rS == rR == rH == (n/4)
+        % Final Answer: House Happiness
+        Final = sortrows(Student_List,6)
+        GR_happy = sum(GR(:,2))/rG
+        SL_happy = sum(SL(:,3))/rS
+        RA_happy = sum(RA(:,4))/rR
+        HU_happy = sum(HU(:,5))/rH
+    else
+        rejects = remove(GR,SL,RA,HU)
+        [GR,SL,RA,HU] = REsort(GR,SL,RA,HU,rejects)
+        break
+    end
+end
 
 
 
@@ -129,14 +177,4 @@ end
 
 
 
-
-
-
-%% Final Answer
-% House Happiness
-Final = sortrows(Student_List,6)
-GR_happy = sum(GR(:,2))/rg
-SL_happy = sum(SL(:,3))/rs
-RA_happy = sum(RA(:,4))/rr
-HU_happy = sum(HU(:,5))/rh
 end
